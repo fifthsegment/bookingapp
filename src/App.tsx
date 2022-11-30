3173bd03a668f1965bc68a15f2a13f3308d34267
fifthsegment <abdullah.irfan1@gmail.com>
2022-11-30 22:38:20 +0100

    Moving to a different direction

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { Refine } from "@pankod/refine-core";
+import {
+  notificationProvider,
+  Layout,
+  ReadyPage,
+  ErrorComponent,
+} from "@pankod/refine-antd";
+
+import dataProvider from "@pankod/refine-simple-rest";
+import "@pankod/refine-antd/dist/styles.min.css";
+import routerProvider from "@pankod/refine-react-router-v6";
+
+function App() {
+  return (
+    <Refine
+      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+      notificationProvider={notificationProvider}
+      Layout={Layout}
+      ReadyPage={ReadyPage}
+      catchAll={<ErrorComponent />}
+      routerProvider={routerProvider}
+    />
+  );
+}
+
+export default App;
